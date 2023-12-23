2db23937bd5524ad9691f083e717344d71dedc78
HP <ahmadadel@>
2023-12-23 15:40:25 +0200

    First Version

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    Button loginButton ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginButton =findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, PostActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
